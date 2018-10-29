b16ab4592248efc3ba23688cf98da45d2e946135
Yuchen <yucliu@163.com>
2018-10-29 21:54:16 +0800

    chart

--- scriptjs.js
+++ scriptjs.js
@@ -1,9 +1,9 @@
 function myfunction(){
     var x = document.getElementById("value").value;
     var y;
-    if ( x = "3"){
+    if ( x == 3){
     var y = "yes! you are right!";}
-     else (x!="3")
-     { var y = "wrong!";}
+     else {
+       var y = "wrong!";}
 document.getElementById("demo").innerHTML = y;
     }
