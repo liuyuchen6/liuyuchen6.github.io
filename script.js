5df53b0babed6bc0cde4ac1a973aac4a2a020b4c
Yuchen <yucliu@163.com>
2018-11-13 00:39:54 +0800

    add markers

---
+++ script.js
@@ -0,0 +1,11 @@
+
+      // create popup contents
+      var customPopup = '<a href="' + data[i].Name + '" target="_blank">' + '<img src="' + data[i].url+'" width = "80px"><br>'
+
+
+      // specify popup options
+      var customOptions =
+          {
+          'maxWidth': '500',
+          'className' : 'custom'
+          }
